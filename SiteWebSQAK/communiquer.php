926f36132ce7d3557372d002ba960fb9392c3ca7
hanakhereba <hanakhereba@yahoo.ca>
2025-03-18 12:41:18 -0400

    3 pages modifier

--- SiteWebSQAK/communiquer.php
+++ SiteWebSQAK/communiquer.php
@@ -1,14 +1,37 @@
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <title>SQAK - Communiquer</title>
     <link rel="stylesheet" type="text/css" href="./css/styles.css">
 </head>
-
 <body>
 
-</body>
+    <div class="content">
+        <form id="communicationForm">
+        <h2 class="section-title">Moyen de communication</h2>
+            <div class="checkbox-group">
+                <input type="checkbox" id="sms"> <label for="sms">SMS</label>
+                <input type="checkbox" id="email"> <label for="email">Courriel</label>
+                <input type="checkbox" id="notification"> <label for="notification">Notification</label>
+            </div>
 
+            <div class="section-title">Destinataires</div>
+            <div class="checkbox-group">
+                <input type="checkbox" id="appliquants"> <label for="appliquants">Appliquants</label>
+                <input type="checkbox" id="benevoles"> <label for="benevoles">Bénévoles</label>
+                <input type="checkbox" id="invites"> <label for="invites">Invités</label>
+            </div>
+
+            <div class="section-title">Message *</div>
+            <textarea id="message" name="message" placeholder="Votre message" required></textarea>
+
+            <div class="buttons">
+                <button type="submit" class="btn btn-send">Envoyer</button>
+                <button type="button" class="btn btn-back" onclick="history.back()">Revenir</button>
+            </div>
+        </form>
+    </div>
+
+</body>
 </html>
--- SiteWebSQAK/modifier-evenement.php
+++ SiteWebSQAK/modifier-evenement.php
@@ -9,6 +9,73 @@
 
 <body>
 
+<body>
+    <div class="container">
+
+        <h2>Modifier événement</h2>
+
+        <form action="#" method="POST">
+
+            <label for="photo">Choisir nouvelle photo:</label>
+            <input type="file" id="photo" name="photo" class="file-input">
+
+            <label for="titre">Titre de l'événement:</label>
+            <input type="text" id="titre" name="titre" required>
+
+            <label for="description">Description:</label>
+            <textarea id="description" name="description" required></textarea>
+
+            <div class="row">
+                <div>
+                    <label for="categorie">Catégorie:</label>
+                    <input type="text" id="categorie" name="categorie" required>
+                </div>
+                <div>
+                    <label for="lieu">Lieu:</label>
+                    <input type="text" id="lieu" name="lieu" required>
+                </div>
+            </div>
+            <div class="row">
+                <div>
+                    <label for="date-debut">Date début:</label>
+                    <input type="date" id="date-debut" name="date-debut" required>
+                </div>
+                <div>
+                    <label for="date-fin">Date fin:</label>
+                    <input type="date" id="date-fin" name="date-fin" required>
+                </div>
+            </div>
+            <div class="row">
+                <div>
+                    <label for="heure-debut">Heure début:</label>
+                    <input type="time" id="heure-debut" name="heure-debut" required>
+                </div>
+                <div>
+                    <label for="heure-fin">Heure fin:</label>
+                    <input type="time" id="heure-fin" name="heure-fin" required>
+                </div>
+            </div>
+            <div class="row">
+                <div>
+                    <label for="benevoles-max">Nombre de bénévoles maximum:</label>
+                    <input type="number" id="benevoles-max" name="benevoles-max">
+                </div>
+                <div>
+                    <label for="invites-max">Nombre d'invités maximum:</label>
+                    <input type="number" id="invites-max" name="invites-max">
+                </div>
+            </div>
+
+            <div class="buttons">
+                <button type="submit" class="btn-save">Modifier</button>
+                <button type="button" class="btn-cancel">Revenir</button>
+            </div>
+        </form>
+    </div>
+
+
+           
+</body>
 </body>
 
 </html>
