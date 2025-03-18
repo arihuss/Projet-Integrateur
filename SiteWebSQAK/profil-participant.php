430e9d394f6c46e6e6a09451d65598f35b792706
sarahhannets <sarah.hann.1@ens.etsmtl.ca>
2025-03-18 11:39:06 -0400

    profil-participant

--- SiteWebSQAK/profil-participant.php
+++ SiteWebSQAK/profil-participant.php
@@ -9,6 +9,30 @@
 
 <body>
 
-</body>
+ <div id="profil-applicant">
+       <h2>Profil de l'applicant </h2>
+       <img src="..\img\profilapplicant.svg" alt="image-profil"> </img>
+       <p id="nom-applicant">Liliane Belvier</p>
+       <div>
+            <p>Courriel:<br>
+               Liliane.belvier@hotmail.com
+            </p>  
+            <p>
+               Numéro de téléphone:<br>
+               514-678-0955
+            </p>
+            <a href="contacter"> Contacter </a>
+        </div>
+</div>
 
+
+<div id="experience-applicant">
+ <h2>Expériences de bénévolat</h2>
+
+
+</div>
+
+
+
+</body>
 </html>
