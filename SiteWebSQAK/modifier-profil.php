90f091dc6f58a6b374d09aef8c3282a9eb897678
arihuss <ariane.hussaini.1@ens.etsmtl.ca>
2025-03-19 08:47:31 -0400

    continuer profil organisateur

--- SiteWebSQAK/modifier-profil.php
+++ SiteWebSQAK/modifier-profil.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>SQAK - Modifier Profil</title>
     <link rel="stylesheet" type="text/css" href="./css/styles.css">
+    <link rel="stylesheet" type="text/css" href="./css/sign-in.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
 </head>
@@ -14,15 +15,58 @@
         <?php
         include("components/header.php")
         ?>
-    </header>
+</header>
 
-    <footer>
+<div class="container">
+    <form action="" method="POST">
+        <h2> Modifier profil </h2>
 
-<?php
-include("components/footer.php");
-?>
+<div id="form-section">
+    <div class="form-column">
+        <h3>Personne</h3>
+        <label for="prenom-ins">Prénom:</label>
+        <input id="prenom-ins" type="text"> 
+        <br>
+        <label for="nom-ins">Nom:</label>
+        <input id="nom-ins" type="text"> 
+    </div>
+
+    <div class="form-column" id="ins-bar">
+         <h3> ou </h3>
+    </div>
+
+    <div class="form-column">
+        <h3>Organisation</h3>
+        <label for="organisation-ins">Nom de l'organisation:</label>
+        <input id="organistaion-ins" type="text"> 
+    </div>
+</div>
+
+    <div id="form-bottom">
+        <label for="courriel-ins">Courriel:</label>
+        <input id="courriel-ins" type="email" required> 
+        <br>
+        <label for="tel-ins">Numéro de téléphone:</label>
+        <input id="tel-ins" type="tel" placeholder="514-222-2222" pattern="^\d{3}-\d{3}-\d{4}$" required> 
+        <br>
+        <label for="mdp-ins">Mot de passe:</label>
+        <input id="mdp-ins" type="text" minlength="8" required> 
+        <br>
+        <label for="Cmdp-ins">Confirmation de mot de passe:</label>
+        <input id="Cmdp-ins" type="text" required> 
+    </div>
+
+    <div id="btn-container">
+        <a class="btn-rose" href="log-in.php">Inscription</a>
+    </div>
+    </form> 
+</div>
+
+<footer>
+    <?php
+    include("components/footer.php");
+    ?>
 </footer>
-
 </body>
 
 </html>
