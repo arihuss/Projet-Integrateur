95e676d0e5e08fc12b9ecd185eb2a704d8747895
sarahhannets <sarah.hann.1@ens.etsmtl.ca>
2025-03-18 21:16:37 -0400

    page sign up

--- SiteWebSQAK/sign-up.php
+++ SiteWebSQAK/sign-up.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>SQAK - Inscription</title>
     <link rel="stylesheet" type="text/css" href="./css/styles.css">
+    <link rel="stylesheet" type="text/css" href="./css/sign-in.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
@@ -13,45 +14,57 @@
         <?php
         include("components/header.php")
         ?>
-    </header>
+</header>
+
+<div class="container">
     <form action="" method="POST">
         <h2> Inscription </h2>
-    <div>
-        <p>Personne</p>
+
+<div id="form-section">
+    <div class="form-column">
+        <h3>Personne</h3>
         <label for="prenom-ins">Prénom:</label>
         <input id="prenom-ins" type="text"> 
+        <br>
         <label for="nom-ins">Nom:</label>
         <input id="nom-ins" type="text"> 
     </div>
 
-    <div id="ins-bar"> <p>ou </p>
+    <div class="form-column" id="ins-bar">
+         <h3> ou </h3>
     </div>
 
-    <div>
-        <p>Organisation</p>
+    <div class="form-column">
+        <h3>Organisation</h3>
         <label for="organisation-ins">Nom de l'organisation:</label>
         <input id="organistaion-ins" type="text"> 
     </div>
+</div>
 
-    <div>
+    <div id="form-bottom">
         <label for="courriel-ins">Courriel:</label>
         <input id="courriel-ins" type="email" required> 
+        <br>
         <label for="tel-ins">Numéro de téléphone:</label>
         <input id="tel-ins" type="tel" placeholder="514-222-2222" pattern="^\d{3}-\d{3}-\d{4}$" required> 
+        <br>
         <label for="mdp-ins">Mot de passe:</label>
         <input id="mdp-ins" type="text" minlength="8" required> 
+        <br>
         <label for="Cmdp-ins">Confirmation de mot de passe:</label>
         <input id="Cmdp-ins" type="text" required> 
     </div>
 
-    <input type="submit" value="Inscription">
- </form> 
+    <div id="btn-container">
+        <a class="btn-rose" href="connexion.php">Inscription</a>
+    </div>
+    </form> 
+</div>
 
- <footer>
-
-<?php
-include("components/footer.php");
-?>
+<footer>
+    <?php
+    include("components/footer.php");
+    ?>
 </footer>
 </body>
 
