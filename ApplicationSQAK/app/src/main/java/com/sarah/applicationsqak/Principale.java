67f9f537f4fca6ac2a00a1347e498feb2a109a6e
romadr199 <desruisseauxroma@gmail.com>
2025-03-24 11:47:05 -0400

    v2.0.1

--- ApplicationSQAK/app/src/main/java/com/sarah/applicationsqak/Principale.java
+++ ApplicationSQAK/app/src/main/java/com/sarah/applicationsqak/Principale.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -26,14 +27,20 @@ public class Principale extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_principale);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         // Liaison des composantes avec la vue
         bottomNav = findViewById(R.id.bottomNavigationView);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
+            bottomNav.setPadding(0, 0, 0, systemBars.bottom);
+            bottomNav.setBackgroundColor(ContextCompat.getColor(this, R.color.navbar));
+            return insets;
+        });
+
+
+
 
         // Get NavHostFragment and ensure its not null
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fgContainer);
