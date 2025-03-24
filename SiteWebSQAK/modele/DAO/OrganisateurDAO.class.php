e57bb9fa6dc9cb96d5ed70ae8d5b0f1026b724d8
arihuss <ariane.hussaini.1@ens.etsmtl.ca>
2025-03-24 15:10:33 -0400

    creer base de organisateur DAO

---
+++ SiteWebSQAK/modele/DAO/OrganisateurDAO.class.php
@@ -0,0 +1,36 @@
+<?php
+include_once(__DIR__ . "../organisateur.class.php");
+
+class OrganisateurDAO {
+
+    /**
+     * Creer un nouvel organisateur au sign-up
+     * @param object $object 
+     * @return bool true si successful
+     */
+    static public function creerOrganisateur(object $object):bool{
+       //a completer
+        return false;
+    }
+
+    /**
+     * Modifier organisateur dans la page modifier
+     * @param object $object
+     * @return bool true si successful
+     */
+    static public function modifierOrganisateur(object $object):bool{
+        //a completer
+        return false;
+    }
+
+    /**
+     * Supprimer le compte de l'organisateur 
+     * @param object $object
+     * @return bool true si successful
+     */
+    static public function deleteOrganisateur(object $object):bool{
+        //a completer
+        return false;
+    }
+}
+?>
