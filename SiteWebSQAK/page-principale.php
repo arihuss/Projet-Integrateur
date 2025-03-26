3a0725cdcbe3aa2e9689d5686c6933f989542241
sarahhannets <sarah.hann.1@ens.etsmtl.ca>
2025-03-25 23:29:33 -0400

    partie hana event

--- SiteWebSQAK/page-principale.php
+++ SiteWebSQAK/page-principale.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>SQAK - Accueil</title>
     <link rel="stylesheet" type="text/css" href="./css/styles.css">
+    <link rel="stylesheet" type="text/css" href="./css/principale.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
 </head>
@@ -14,23 +15,29 @@
 <header><?php include("components/header.php");?></header>
 
 <div class="container">
-    <div class="content">
-        <h2>Mes événements</h2>
-    
-        <button class="add-button" onclick="window.location.href='ajouter-evenement.php'">Ajouter</button>
+    <div>
+        
+    <div class="header-row">
+    <h2>Mes événements</h2>
+    <button class="add-button" onclick="window.location.href='ajouter-evenement.php'">Ajouter</button>
+</div>
 
+
+    <a href="evenement.php" class="event">
         <div class="event-grid">
             <div class="event-card">
                 <div class="event-title">Entretient d'arbres</div>
-                <img src=".\img\event-arbre.svg" alt="img-evenement">">
+                <img src=".\img\event-arbre.svg" alt="img-evenement">
                 <div class="event-date">13/12/2025</div>
             </div>
+    </a>
 
             <div class="event-card">
                 <div class="event-title">Entretient d'arbres</div>
-                <img src=".\img\event-arbre.svg" alt="img-evenement">">
+                <img src=".\img\event-arbre.svg" alt="img-evenement">
                 <div class="event-date">13/12/2025</div>
             </div>
+            
 
             <div class="event-card">
                 <div class="event-title">Entretient d'arbres</div>
@@ -65,4 +72,3 @@
 </body>
 
 </html>
-
