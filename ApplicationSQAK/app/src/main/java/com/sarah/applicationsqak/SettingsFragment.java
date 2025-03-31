6b925885dacf14745b0b68935c021094e59fdbff
nllz75 <nll.75pro@gmail.com>
2025-03-31 01:37:23 -0400

    test boutons modifier profil

--- ApplicationSQAK/app/src/main/java/com/sarah/applicationsqak/SettingsFragment.java
+++ ApplicationSQAK/app/src/main/java/com/sarah/applicationsqak/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.sarah.applicationsqak;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +25,7 @@ public class SettingsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    Button btnModifier, btnPlus;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -58,7 +61,36 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        btnModifier = view.findViewById(R.id.btnModifProfil);
+        btnPlus = view.findViewById(R.id.btnPlus);
+
+        btnModifier.setOnClickListener(this);
+        btnPlus.setOnClickListener(this);
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        return view;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == btnModifier) {
+
+            Intent intention1;
+            intention1 = new Intent(getActivity(), ModifierProfil.class);
+            startActivity(intention1);
+
+        } else if (v ==btnPlus){
+
+            Intent intention2;
+            intention2 = new Intent(getActivity(), AboutUs.class);
+            startActivity(intention2);
+        }
+
     }
 }
