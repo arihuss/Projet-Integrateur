83fb19442a336a2939641ad05e5452025098c5da
arihuss <ariane.hussaini.1@ens.etsmtl.ca>
2025-04-02 22:34:35 -0400

    dao commentaires
    forgot to give it out

--- SiteWebSQAK/modele/DAO/CommentaireDAO.class.php
+++ SiteWebSQAK/modele/DAO/CommentaireDAO.class.php
@@ -10,10 +10,36 @@ class CommentaireDAO{
      * @return array les commentaires
      */
     static public function findByEvenement(int $id_evenement):array{
-        //a completer
-         return [0];
-     }
+        try {
+            $connexion = ConnexionBD::getInstance();
+        } catch (Exception $e) {
+            throw new Exception("Impossible d'obtenir la connexion à la BD");
+        }
 
-   
+        $commentaires = [];
+        $requete = $connexion->prepare(
+            "SELECT C.*
+             FROM Commentaire C
+             JOIN Evenement E ON C.id_evenement = E.id_evenement"
+        );
+        $requete->bindParam(':id_evenement', $id_evenement, PDO::PARAM_STR);
+        $requete->execute();
+
+        foreach($requete as $enr){
+            $commentaires[] = new Commentaire(
+                $enr['id_commentaire'],
+                $enr['id_utilisateur'],
+                $enr['id_evenement'],
+                $enr['message'],
+                $enr['date_envoi'],
+            );
+        }
+
+        $requete->closeCursor();
+        ConnexionBD::close();
+
+        return $commentaires;
+    
+     } 
 }
 ?>
