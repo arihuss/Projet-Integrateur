d22e3b71e2c750456d3e27c5828e4277a57a2f1c
romadr199 <desruisseauxroma@gmail.com>
2025-04-07 11:44:01 -0400

    update gradle pour utiliser 'Glide'
    ca ne fonctionne toujours pas, mis en commentaire

--- ApplicationSQAK/app/build.gradle.kts
+++ ApplicationSQAK/app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+    // alias(libs.plugins.kotlin.kapt)
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -43,5 +45,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    //kapt("com.github.bumptech.glide:compiler:4.16.0")
+    //implementation(libs.glide)
+    //kapt(libs.compiler)
 }
 
