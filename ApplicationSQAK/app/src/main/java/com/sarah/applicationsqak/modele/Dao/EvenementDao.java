8a827a7062fb11e7c09fabf33fab6f86fcc92920
romadr199 <desruisseauxroma@gmail.com>
2025-04-13 15:15:18 -0400

    evenement dao

--- ApplicationSQAK/app/src/main/java/com/sarah/applicationsqak/modele/Dao/EvenementDao.java
+++ ApplicationSQAK/app/src/main/java/com/sarah/applicationsqak/modele/Dao/EvenementDao.java
@@ -1,4 +1,37 @@
 package com.sarah.applicationsqak.modele.Dao;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.sarah.applicationsqak.modele.Evenement;
+import com.sarah.applicationsqak.modele.sqlite.BaseContrat;
+import com.sarah.applicationsqak.modele.sqlite.DbUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EvenementDao {
+    private DbUtil dbUtil;
+
+    public EvenementDao(Context context) {
+        dbUtil = new DbUtil(context);
+    }
+
+    public List<Evenement> getEvenements() {
+        List<Evenement> evenements = new ArrayList<>();
+        SQLiteDatabase db = dbUtil.getReadableDatabase();
+
+        Cursor cursor = db.query(BaseContrat.EvenementTable.TABLE_NAME, null, null, null, null, null, null);
+        if(cursor != null) {
+            cursor.moveToFirst();
+
+            // Remplir la liste
+            do {
+                Evenement e = new Evenement();
+            }while(cursor.moveToNext());
+        }
+
+        return cursor;
+    }
 }
