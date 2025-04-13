4adfc844c2e476c643cd444d9f00535f7bbb512e
romadr199 <desruisseauxroma@gmail.com>
2025-04-13 11:40:07 -0400

    sqlite

---
+++ ApplicationSQAK/app/src/main/java/sqlite/BaseContrat.java
@@ -0,0 +1,105 @@
+package sqlite;
+
+import android.provider.BaseColumns;
+
+public class BaseContrat {
+    public static final String DB_NAME = "SQAK.DB";
+    public static final int DB_VERSION = 1;
+
+
+    // Colonnes de chaque table
+    public static final class UtilisateurTable {
+        public static final String TABLE_NAME = "Utilisateur";
+        public static final String ID_UTILISATEUR = BaseColumns._ID;
+        public static final String PRENOM = "PRENOM";
+        public static final String NOM = "NOM";
+        public static final String COURRIEL = "COURRIEL";
+        public static final String NUM_TEL = "NUM_TEL";
+        public static final String BIO = "BIO";
+        public static final String MOT_DE_PASSE = "MOT_DE_PASSE";
+    }
+
+    public static final class OrganisateurTable {
+        public static final String TABLE_NAME = "Organisateur";
+        public static final String ID_ORGANISATEUR = BaseColumns._ID;
+        public static final String PRENOM = "PRENOM";
+        public static final String NOM = "NOM";
+        public static final String COURRIEL = "COURRIEL";
+        public static final String NOM_ORGANISATEUR = "NOM_ORGANISATEUR";
+        public static final String BIO = "BIO";
+        public static final String MOT_DE_PASSE = "MOT_DE_PASSE";
+        public static final String NB_EVENTS = "NB_EVENTS";
+    }
+
+
+    public static final class StatistiqueTable {
+        public static final String TABLE_NAME = "Statistique";
+        public static final String ID_STATISTIQUE = BaseColumns._ID;
+        public static final String NB_VISITEURS = "NB_VISITEURS";
+        public static final String NB_BENEVOLES = "NB_BENEVOLES";
+        public static final String NB_LIKES = "NB_LIKES";
+        public static final String NB_VUES = "NB_VUES";
+        public static final String NB_PARTAGES = "NB_PARTAGES";
+
+    }
+
+    public static final class EvenementTable {
+        public static final String TABLE_NAME = "Evenement";
+        public static final String ID_EVENEMENT = BaseColumns._ID;
+        public static final String ID_ORGANISATEUR = "ID_ORGANISATEUR";
+        public static final String ID_STATISTIQUE = "ID_STATISTIQUE";
+        public static final String NOM_EVENT = "NOM_EVENT";
+        public static final String LIEU = "LIEU";
+        public static final String DATE_DEBUT = "DATE_DEBUT";
+        public static final String DATE_FIN = "DATE_FIN";
+        public static final String NB_BENEVOLES_MAX = "NB_BENEVOLES_MAX";
+        public static final String NB_PARTICIPANTS_MAX = "NB_PARTICIPANTS_MAX";
+        public static final String ETAT_BENEVOLE = "ETAT_BENEVOLE";
+        public static final String CATEGORIE = "CATEGORIE";
+        public static final String DESCRIPTION = "DESCRIPTION";
+        public static final String ETAT = "ETAT";
+        public static final String NB_INSCRIPTIONS = "NB_INSCRIPTIONS";
+        public static final String NB_BENEVOLES_ACCEPTES = "NB_BENEVOLES_ACCEPTES";
+        public static final String COMPLET_BENEVOLE = "COMPLET_BENEVOLE";
+        public static final String COMPLET_VISITEUR = "COMPLET_VISITEUR";
+
+
+    }
+
+
+    public static final class InscriptionTable {
+        public static final String TABLE_NAME = "Inscription";
+        public static final String ID_INSCRIPTION = BaseColumns._ID;
+        public static final String ID_UTILISATEUR = "ID_UTILISATEUR";
+        public static final String ID_EVENEMENT = "ID_EVENEMENT";
+        public static final String ROLE = "ROLE";
+        public static final String DATE_INSCRIPTION = "DATE_INSCRIPTION";
+        public static final String DATE_ANNULATION = "DATE_ANNULATION";
+
+    }
+
+    public static final class CommentaireTable {
+        public static final String TABLE_NAME = "Commentaire";
+        public static final String ID_COMMENTAIRE = BaseColumns._ID;
+        public static final String ID_UTILISATEUR = "ID_UTILISATEUR";
+        public static final String ID_EVENEMENT = "ID_EVENEMENT";
+        public static final String MESSAGE = "MESSAGE";
+        public static final String DATE_ENVOI = "DATE_ENVOI";
+
+    }
+
+    public static final class MessageTable {
+        public static final String TABLE_NAME = "Message";
+        public static final String ID_MESSAGE = BaseColumns._ID;
+        public static final String ID_EVENEMENT = "ID_EVENEMENT";
+        public static final String MOYEN_COMMUNICATION = "MOYEN_COMMUNICATION";
+        public static final String TYPE_DESTINATAIRE = "TYPE_DESTINATAIRE";
+        public static final String MESSAGE = "MESSAGE";
+        public static final String DATE_ENVOI = "DATE_ENVOI";
+
+
+    }
+
+
+}
+}
--- ApplicationSQAK/app/src/main/java/sqlite/DBUtil.java
+++ ApplicationSQAK/app/src/main/java/sqlite/DBUtil.java
@@ -7,22 +7,85 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBUtil extends SQLiteOpenHelper {
 
     public DBUtil(Context context) {
-        super(context, Event.DB_NAME, null, Event.DB_VERSION);
+        super(context, BaseContrat.DB_NAME, null, BaseContrat.DB_VERSION);
+    }
+
+
+    @Override
+    public void onConfigure(SQLiteDatabase db) {
+        // Active les contraintes de clé étrangère
+        super.onConfigure(db);
+        db.setForeignKeyConstraintsEnabled(true);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String requeteCreation = String.format("Create table %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s text, %S double)",
-        Event.TABLE_NAME,
-        Event.Colonnes.ID,
-        Event.Colonnes.NOM);
-    db.execSQL(requeteCreation);
+        // Création des tables
+        String requeteCreationUtilisateur = String.format("Create table %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s text, %s text, %s text unique, %s text unique, %s text, %s text CHECK (length (%s)>= 8))",
+            BaseContrat.UtilisateurTable.TABLE_NAME,
+            BaseContrat.UtilisateurTable.ID_UTILISATEUR,
+            BaseContrat.UtilisateurTable.PRENOM,
+            BaseContrat.UtilisateurTable.NOM,
+            BaseContrat.UtilisateurTable.COURRIEL,
+            BaseContrat.UtilisateurTable.NUM_TEL,
+            BaseContrat.UtilisateurTable.BIO,
+            BaseContrat.UtilisateurTable.MOT_DE_PASSE,
+            BaseContrat.UtilisateurTable.MOT_DE_PASSE);
+        db.execSQL(requeteCreationUtilisateur);
+
+        String requeteCreationOrganisateur = String.format("Create table %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s text, %s text, %s text unique, %s text, %s text, %s text, %s integer default 0 CHECK (length (%s)>= 8))",
+                BaseContrat.OrganisateurTable.TABLE_NAME,
+                BaseContrat.OrganisateurTable.ID_ORGANISATEUR,
+                BaseContrat.OrganisateurTable.PRENOM,
+                BaseContrat.OrganisateurTable.NOM,
+                BaseContrat.OrganisateurTable.COURRIEL,
+                BaseContrat.OrganisateurTable.BIO,
+                BaseContrat.OrganisateurTable.NOM_ORGANISATEUR,
+                BaseContrat.OrganisateurTable.MOT_DE_PASSE,
+                BaseContrat.OrganisateurTable.NB_EVENTS,
+                BaseContrat.OrganisateurTable.MOT_DE_PASSE);
+        db.execSQL(requeteCreationOrganisateur);
+
+        String requeteCreationStatistique = String.format("Create table %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s integer default 0, %s integer default 0, %s integer default 0, %s integer default 0, %s integer default 0)",
+                BaseContrat.StatistiqueTable.TABLE_NAME,
+                BaseContrat.StatistiqueTable.ID_STATISTIQUE,
+                BaseContrat.StatistiqueTable.NB_VISITEURS,
+                BaseContrat.StatistiqueTable.NB_BENEVOLES,
+                BaseContrat.StatistiqueTable.NB_LIKES,
+                BaseContrat.StatistiqueTable.NB_VUES,
+                BaseContrat.StatistiqueTable.NB_PARTAGES);
+        db.execSQL(requeteCreationStatistique);
+
+        String requeteCreationEvenement = String.format("Create table %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s integer , %s integer , %s text, %s text, %s text, %s text, %s integer default 0, " +
+                        "%s integer default 0, %s integer check (%s IN (0,1)), %s text, %s text, %s text check (%s IN ('disponible', 'termine')), %s integer default 0, %s integer default 0, %s integer check (%s IN (0,1)), " +
+                        "%s integer check (%s IN (0,1)), FOREIGN KEY (%s) REFERENCES %s (%s) ON DELETE CASCADE, FOREIGN KEY (%s) REFERENCES %s (%s) ON DELETE CASCADE)",
+                BaseContrat.EvenementTable.TABLE_NAME,
+                BaseContrat.EvenementTable.ID_EVENEMENT,
+                BaseContrat.EvenementTable.ID_STATISTIQUE,
+                BaseContrat.EvenementTable.ID_ORGANISATEUR,
+                BaseContrat.EvenementTable.NOM_EVENT,
+                BaseContrat.EvenementTable.LIEU,
+                BaseContrat.EvenementTable.DATE_DEBUT,
+                BaseContrat.EvenementTable.DATE_FIN,
+                BaseContrat.EvenementTable.NB_BENEVOLES_MAX,
+                BaseContrat.EvenementTable.NB_PARTICIPANTS_MAX,
+                BaseContrat.EvenementTable.ETAT_BENEVOLE, BaseContrat.EvenementTable.ETAT_BENEVOLE,
+                BaseContrat.EvenementTable.CATEGORIE,
+                BaseContrat.EvenementTable.DESCRIPTION,
+                BaseContrat.EvenementTable.ETAT, BaseContrat.EvenementTable.ETAT,
+                BaseContrat.EvenementTable.NB_INSCRIPTIONS,
+                BaseContrat.EvenementTable.NB_BENEVOLES_ACCEPTES,
+                BaseContrat.EvenementTable.COMPLET_BENEVOLE, BaseContrat.EvenementTable.COMPLET_BENEVOLE,
+                BaseContrat.EvenementTable.COMPLET_VISITEUR, BaseContrat.EvenementTable.COMPLET_VISITEUR,
+                BaseContrat.EvenementTable.ID_ORGANISATEUR, BaseContrat.OrganisateurTable.TABLE_NAME, BaseContrat.OrganisateurTable.ID_ORGANISATEUR,
+                BaseContrat.EvenementTable.ID_STATISTIQUE, BaseContrat.StatistiqueTable.TABLE_NAME, BaseContrat.StatistiqueTable.ID_STATISTIQUE);
+        db.execSQL(requeteCreationEvenement);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         String requeteModification = String.format("alter table %s ADD %s int not null",
-                Event.TABLE_NAME, "DESCRIPTION");
+                //BaseContrat.TABLE_NAME, "DESCRIPTION");
         db.execSQL(requeteModification);
     }
 }
